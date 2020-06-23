1608370dab2d673107cf3e390745d18e45e75382
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-22 17:03:47 -0700

    Updates

---
+++ precache-manifest.45064d005411fcf999b848612ccf731f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc9706c690adf2a56b71cbb544505dc",
+    "url": "/todo-client-react/index.html"
+  },
+  {
+    "revision": "a9d623c7c56900d9c20b",
+    "url": "/todo-client-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bea9472db4ed4f0a6451",
+    "url": "/todo-client-react/static/js/2.1ac981e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-client-react/static/js/2.1ac981e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d623c7c56900d9c20b",
+    "url": "/todo-client-react/static/js/main.876836ac.chunk.js"
+  },
+  {
+    "revision": "c8df1b1c30bbeb8203b3",
+    "url": "/todo-client-react/static/js/runtime-main.f74c74e5.js"
+  }
+]);
