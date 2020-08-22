671120870450f00922d886a2e163007e4a54093b
BackToTheStars <nikgrigoryev@gmail.com>
2020-08-22 15:47:42 -0700

    Added and corrected links

--- src/Links.js
+++ src/Links.js
@@ -11,31 +11,46 @@ function Links() {
 
       <h4>Project: ToDo list, connected to my Mongo Atlas DB, w/ backend deployed on Heroku</h4>
 
-      <p>Links:</p>
+      <p>Client links:</p>
 
-      <p id="link"><span>{'Client Github website: '}</span>
+      <p id="link"><span>{'Github website: '}</span>
         <a href="http://BackToTheStars.github.io/todo-client-react">
           http://BackToTheStars.github.io/todo-client-react
         </a>
       </p>
 
-      <p id="link"><span>{'Client Github repository: '}</span>
+      <p id="link"><span>{'Github repository: '}</span>
         <a href="https://github.com/BackToTheStars/todo-client-react/tree/master/src">
           https://github.com/BackToTheStars/todo-client-react/tree/master/src
         </a>
       </p>
 
-      <p id="link"><span>{'Backend Heroku website: '}</span>
+      <p id="link"><span>{'Local folder: '}</span>
+        <a href="D:\React\0050-todo-client">
+          D:\React\0050-todo-client
+        </a>
+      </p>
+
+      <p>Backend links:</p>
+
+      <p id="link"><span>{'Heroku website: '}</span>
         <a href="https://to-do-server-heroku-092.herokuapp.com">
           https://to-do-server-heroku-092.herokuapp.com
         </a>
       </p>
 
-      <p id="link"><span>{'Backend Github repository: '}</span>
+      <p id="link"><span>{'Github repository: '}</span>
         <a href="https://github.com/BackToTheStars/todo-server-myown-remote-mongodb-checked">
           https://github.com/BackToTheStars/todo-server-myown-remote-mongodb-checked
         </a>
       </p>
+
+      <p id="link"><span>{'Local folder: '}</span>
+        <a href="D:\React\0101-my-own-todo-server-remote-atlas">
+          D:\React\0101-my-own-todo-server-remote-atlas
+        </a>
+      </p>
+
       <br />
     </div>
   );
