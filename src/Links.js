6f605a81b208fa07ca847998395a1c26c6947256
BackToTheStars <nikgrigoryev@gmail.com>
2020-08-22 15:26:15 -0700

    Some little corrections

--- src/Links.js
+++ src/Links.js
@@ -5,20 +5,36 @@ function Links() {
 
   return (
     <div>
-      <p id="date">July 3, 2020</p>
-      <h4>Project: ToDo list, connected to Mongo Atlas DB</h4>
+      <p id="date">Created: July 3, 2020</p>
+      <p id="date">Updated: August 22, 2020</p>
+      <br/>
+
+      <h4>Project: ToDo list, connected to my Mongo Atlas DB, w/ backend deployed on Heroku</h4>
+
       <p>Links:</p>
+
       <p id="link"><span>{'Client Github website: '}</span>
-        <a href="http://BackToTheStars.github.io/todo-client-react">http://BackToTheStars.github.io/todo-client-react</a>
+        <a href="http://BackToTheStars.github.io/todo-client-react">
+          http://BackToTheStars.github.io/todo-client-react
+        </a>
       </p>
+
       <p id="link"><span>{'Client Github repository: '}</span>
-        <a href="https://github.com/BackToTheStars/todo-client-react/tree/master/src">https://github.com/BackToTheStars/todo-client-react/tree/master/src</a>
+        <a href="https://github.com/BackToTheStars/todo-client-react/tree/master/src">
+          https://github.com/BackToTheStars/todo-client-react/tree/master/src
+        </a>
       </p>
+
       <p id="link"><span>{'Backend Heroku website: '}</span>
-        <a href="https://to-do-server-heroku-092.herokuapp.com/todo">https://to-do-server-heroku-092.herokuapp.com/todo</a>
+        <a href="https://to-do-server-heroku-092.herokuapp.com">
+          https://to-do-server-heroku-092.herokuapp.com
+        </a>
       </p>
+
       <p id="link"><span>{'Backend Github repository: '}</span>
-        <a href="https://github.com/BackToTheStars/0092-todo-server-heroku">https://github.com/BackToTheStars/0092-todo-server-heroku</a>
+        <a href="https://github.com/BackToTheStars/todo-server-myown-remote-mongodb-checked">
+          https://github.com/BackToTheStars/todo-server-myown-remote-mongodb-checked
+        </a>
       </p>
       <br />
     </div>
